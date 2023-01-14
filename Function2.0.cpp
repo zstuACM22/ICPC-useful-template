79271aa9f06614191fc029a3bda1020ba660a939
Jamhus Tao <jamhus_tao@qq.com>
2023-01-15 02:35:43 +0800

    Add one function graph::lowest_common_ancestor::ancestor()

--- Function2.0.cpp
+++ Function2.0.cpp
@@ -3368,14 +3368,16 @@ namespace graph
     // 最近公共祖先. 时间复杂度: O(logn) + 预处理: O(nlogn), 空间复杂度: O(nlogn)
     namespace lowest_common_ancestor
     {
+        // 1-index
         const int MAX = 100005;
         const int LOG = 17;
 
         int lg2[MAX];
         vector<int> edge[MAX];
-        int depth[MAX];
+        int depth[MAX];  // 根深为 0
         int st[MAX][LOG];
 
+        // 预处理对数
         void load_lg2(int n)
         {
             lg2[0] = -1;
@@ -3384,7 +3386,7 @@ namespace graph
         }
 
         // 建表
-        void dfs(int x, int pre, int level)
+        void build(int x, int pre, int level)
         {
             depth[x] = level;
             st[x][0] = pre;
@@ -3392,7 +3394,18 @@ namespace graph
                 st[x][i] = st[st[x][i - 1]][i - 1];
             for (int y : edge[x])
                 if (y != pre)
-                    dfs(y, x, level + 1);
+                    build(y, x, level + 1);
+        }
+
+        // 查询 x 的 p 级祖先, 不存在返回 -1
+        int ancestor(int x, int p) {
+            if (p > depth[x]) return -1;
+            while(p) {
+                int step = lg2[p];
+                x = st[x][step];
+                p -= 1 << step;
+            }
+            return x;
         }
 
         // 查询
