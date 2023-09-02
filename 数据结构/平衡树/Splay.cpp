28060227f308c0f1135d78797dd1e8e9e1ae182f
Jamhus_tao <jamhus_tao@qq.com>
2023-09-02 17:08:37 +0800

    20230902

---
+++ 数据结构/平衡树/Splay.cpp
@@ -0,0 +1,304 @@
+// Jamhus Tao / GreatLiangpi
+// Start at 2022/10/6
+// Please using int32_t and int64_t to replace the int and long long.
+#include <bits/stdc++.h>
+#define int int64_t
+#define endl '\n'
+#pragma GCC optimize(3, "Ofast", "inline")
+using namespace std;
+
+// Splay 平衡树
+const int MAX = 100005;
+const int INF = 0x3f3f3f3f3f3f3f3fll;
+
+// 代码实现约定:
+// tr[0].sum 始终为 0, tr[0] 其余随意; tr[root].father 始终为 0; root == 0 表示空树, next == 0 表示无儿子
+struct Splay {
+    int key;      // 键
+    int cnt;      // 计数
+    int father;   // 父节点
+    int next[2];  // 左右子节点
+    int sum;      // 子树求和
+} tr[MAX];        // 1-index
+int root = 0;
+int cnt_tr = 1;
+
+// 清空
+void clear() {
+    memset(tr, 0, sizeof(Splay) * cnt_tr);
+    cnt_tr = 1;
+}
+
+// 维护子树求和, idx != 0
+inline void up(int idx) { tr[idx].sum = tr[tr[idx].next[0]].sum + tr[tr[idx].next[1]].sum + tr[idx].cnt; }
+
+// 为父的右儿子, idx != root and idx != 0
+inline bool who(int idx) { return idx == tr[tr[idx].father].next[1]; }
+
+// 旋转使 idx 上升, idx != root and idx != 0
+inline void rotate(int idx) {
+    bool w = who(idx);
+    int f = tr[idx].father, ff = tr[f].father, s = tr[idx].next[not w];
+    tr[ff].next[who(f)] = idx;
+    tr[idx].father = ff;
+    tr[f].next[w] = s;
+    tr[s].father = f;
+    tr[idx].next[not w] = f;
+    tr[f].father = idx;
+    up(f);
+    up(idx);
+}
+
+// splay 核心操作, 分 zig / zig-zig / zig-zag, 所有指针移动后都要 splay. tar 表示 splay 到变为谁的子节点
+inline void splay(int idx, int tar = 0) {
+    for (int f = tr[idx].father; f != tar; f = tr[idx].father) {
+        if (tr[f].father != tar)
+            rotate(who(idx) == who(f) ? f : idx);
+        rotate(idx);
+    }
+    if (tar == 0)
+        root = idx;
+}
+
+typedef int iter;  // 索引, 避免与值混淆
+
+// 建树, 0-index. 时间: O(nlogn)
+int a[MAX];
+void build(int n) {
+    if (n == 0) 
+        return;
+    sort(a, a + n);  // 瓶颈
+    int pre = -INF - 1;
+    for (int i = 0; i < n; i++) {
+        if (a[i] == pre) {
+            tr[cnt_tr - 1].cnt++;
+        } else {
+            tr[cnt_tr].key = a[i];
+            tr[cnt_tr].cnt = 1;
+            tr[cnt_tr - 1].next[1] = cnt_tr;
+            tr[cnt_tr].father = cnt_tr - 1;
+            cnt_tr++;
+        }
+        pre = a[i];
+    }
+    for (int i = cnt_tr - 1; i >= 1; i--)
+        up(i);
+    root = 1;
+}
+
+// 遍历. 时间: O(n)
+iter walk_list[MAX];
+int walk_cnt = 0;
+void walk(iter idx) {
+    if (tr[idx].next[0]) walk(tr[idx].next[0]);
+    walk_list[walk_cnt++] = idx;
+    if (tr[idx].next[1]) walk(tr[idx].next[1]);
+}
+
+// 元素索引, 元素不存在返回 -1. 时间: O(logn)
+iter find(int x) {
+    if (root == 0)
+        return -1;
+    int idx = root;
+    while (true) {
+        if (x == tr[idx].key) {
+            splay(idx);
+            return idx;
+        }
+        if (tr[idx].next[x > tr[idx].key] == 0) {
+            splay(idx);
+            return -1;
+        }
+        idx = tr[idx].next[x > tr[idx].key];
+    }
+}
+
+// 最小元素索引, 空树返回 -1. 时间: O(logn)
+iter mini() {
+    if (root == 0)
+        return -1;
+    int idx = root;
+    while (tr[idx].next[0])
+        idx = tr[idx].next[0];
+    splay(idx);
+    return idx;
+}
+
+// 最大元素索引, 空树返回 -1. 时间: O(logn)
+iter maxi() {
+    if (root == 0)
+        return -1;
+    int idx = root;
+    while (tr[idx].next[1])
+        idx = tr[idx].next[1];
+    splay(idx);
+    return idx;
+}
+
+// 元素前驱索引. 特别地, 无前驱返回 -1, -1 前驱返回最大元素索引. 时间: O(logn)
+iter pre(iter idx) {
+    if (idx == -1)
+        return maxi();
+    splay(idx);
+    idx = tr[idx].next[0];
+    if (idx == 0)
+        return -1;
+    while (tr[idx].next[1])
+        idx = tr[idx].next[1];
+    splay(idx);
+    return idx;
+}
+
+// 元素后继索引. 特别地, 无后继返回 -1, -1 后继返回最小元素索引. 时间: O(logn)
+iter nxt(iter idx) {
+    if (idx == -1)
+        return mini();
+    splay(idx);
+    idx = tr[idx].next[1];
+    if (idx == 0)
+        return -1;
+    while (tr[idx].next[0])
+        idx = tr[idx].next[0];
+    splay(idx);
+    return idx;
+}
+
+// 插入元素. 时间: O(logn)
+void add(int x) {
+    // case 1: 空树
+    if (root == 0) {
+        int nxt = cnt_tr++;
+        tr[nxt].key = x;
+        tr[nxt].cnt = 1;
+        tr[nxt].father = 0;
+        up(nxt);
+        splay(nxt);
+        return;
+    }
+    int idx = root;
+    while (true) {
+        // case 2: 元素已存在
+        if (x == tr[idx].key) {
+            tr[idx].cnt++;
+            up(idx);
+            splay(idx);
+            return;
+        }
+        // case 3: 元素不存在
+        if (tr[idx].next[x > tr[idx].key] == 0) {
+            int nxt = cnt_tr++;
+            tr[idx].next[x > tr[idx].key] = nxt;
+            tr[nxt].key = x;
+            tr[nxt].cnt = 1;
+            tr[nxt].father = idx;
+            up(nxt);
+            up(idx);
+            splay(nxt);
+            return;
+        }
+        idx = tr[idx].next[x > tr[idx].key];
+    }
+}
+
+// 删除元素, 元素不存在返回 -1. 时间: O(logn)
+bool del(int x) {
+    // case 1: 空树
+    if (root == 0)
+        return false;
+    int idx = root;
+    while (true) {
+        if (x == tr[idx].key)
+            break;
+        // case 2: 元素不存在
+        if (tr[idx].next[x > tr[idx].key] == 0) {
+            splay(idx);
+            return false;
+        }
+        idx = tr[idx].next[x > tr[idx].key];
+    }
+    tr[idx].cnt--;
+    up(idx);
+    splay(idx);
+    // case 3: 无需移除点
+    if (tr[idx].cnt)
+        return true;
+    // case 4: 移除后左子树为空
+    if (tr[idx].next[0] == 0) {
+        root = tr[idx].next[1];
+        tr[root].father = 0;
+        if (root)
+            up(root);
+        return true;
+    }
+    // case 5: 移除后左子树非空
+    root = tr[idx].next[0];
+    tr[root].father = 0;
+    maxi();
+    int s = tr[idx].next[1];
+    tr[root].next[1] = s;
+    tr[s].father = root;
+    up(root);
+    return true;
+}
+
+// 元素 x 排名, x 不存在返回 -1. 1-index, 时间: O(logn)
+int rk(int x) {
+    if (root == 0)
+        return -1;
+    int idx = root, cnt = 0;
+    while (idx) {
+        if (x < tr[idx].key) {
+            idx = tr[idx].next[0];
+        } else {
+            cnt += tr[tr[idx].next[0]].sum;
+            if (x == tr[idx].key) {
+                splay(idx);
+                return cnt + 1;
+            }
+            idx = tr[idx].next[1];
+        }
+    }
+    return -1;
+}
+
+// 排名 rk 索引, 总数不足 rk 返回 -1. 1-index, 时间: O(logn)
+iter kth(int rk) {
+    if (tr[root].sum < rk)
+        return -1;
+    int idx = root, cnt = 0;
+    while (true) {
+        if (tr[tr[idx].next[0]].sum >= rk) {
+            idx = tr[idx].next[0];
+        } else {
+            rk -= tr[tr[idx].next[0]].sum + tr[idx].cnt;
+            if (rk <= 0) {
+                splay(idx);
+                return idx;
+            }
+            idx = tr[idx].next[1];
+        }
+    }
+}
+
+// 第一个大于等于 x 的元素索引, 不存在返回 -1. 时间: O(logn)
+iter lower_bound(int x) {
+    if (root == 0)
+        return -1;
+    int idx = root;
+    while (true) {
+        if (x == tr[idx].key) {
+            splay(idx);
+            return idx;
+        }
+        if (tr[idx].next[x > tr[idx].key] == 0) {
+            splay(idx);
+            if (x < tr[idx].key)
+                return idx;
+            return nxt(idx);
+        }
+        idx = tr[idx].next[x > tr[idx].key];
+    }
+}
+
+// 第一个严格大于 x 的元素索引, 不存在返回 -1. 时间: O(logn)
+iter upper_bound(int x) { return lower_bound(x + 1); }
---
+++ 经典/Splay 区间翻转 - 文艺平衡树.cpp
@@ -0,0 +1,159 @@
+// Jamhus Tao / GreatLiangpi
+// Start at 2022/10/6
+// Please using int32_t and int64_t to replace the int and long long.
+#include <bits/stdc++.h>
+#define int int64_t
+#define endl '\n'
+#pragma GCC optimize(3, "Ofast", "inline")
+using namespace std;
+
+/*
+# 【模板】文艺平衡树
+
+## 题目描述
+
+您需要写一种数据结构（可参考题目标题），来维护一个有序数列。  
+
+其中需要提供以下操作：翻转一个区间，例如原有序序列是 $5\ 4\ 3\ 2\ 1$，翻转区间是 $[2,4]$ 的话，结果是 $5\ 2\ 3\ 4\ 1$。
+
+## 输入格式
+
+第一行两个正整数 $n,m$，表示序列长度与操作个数。序列中第 $i$ 项初始为 $i$。  
+接下来 $m$ 行，每行两个正整数 $l,r$，表示翻转的区间。
+
+## 输出格式
+
+输出一行 $n$ 个正整数，表示原始序列经过 $m$ 次变换后的结果。
+
+## 样例 #1
+
+### 样例输入 #1
+
+```
+5 3
+1 3
+1 3
+1 4
+```
+
+### 样例输出 #1
+
+```
+4 3 2 1 5
+```
+
+## 提示
+
+【数据范围】  
+对于 $100\%$ 的数据，$1 \le n, m \leq 100000 $，$1 \le l \le r \le n$。
+*/
+
+const int MAX = 100005;
+
+struct Splay {
+    int father;
+    int next[2];
+    int sum;
+    bool lazy;
+} tr[MAX];  // 1-index
+int root = 0;
+int cnt_tr = 1;
+
+void clear() {
+    memset(tr, 0, sizeof(Splay) * cnt_tr);
+    cnt_tr = 1;
+}
+
+inline void up(int x) { tr[x].sum = tr[tr[x].next[0]].sum + tr[tr[x].next[1]].sum + 1; }
+
+inline void done(int x) {
+    if (tr[x].lazy) {
+        int &li = tr[x].next[0];
+        int &ri = tr[x].next[1];
+        tr[li].lazy ^= true;
+        tr[ri].lazy ^= true;
+        swap(li, ri);
+        tr[x].lazy = false;
+    }
+}
+
+inline bool who(int x) { return x == tr[tr[x].father].next[1]; }
+
+inline void rotate(int x) {
+    bool w = who(x);
+    int f = tr[x].father, ff = tr[f].father, s = tr[x].next[not w];
+    tr[ff].next[who(f)] = x;
+    tr[x].father = ff;
+    tr[f].next[w] = s;
+    tr[s].father = f;
+    tr[x].next[not w] = f;
+    tr[f].father = x;
+    up(f);
+    up(x);
+}
+
+inline void splay(int x, int tar) {
+    for (int f = tr[x].father; f != tar; f = tr[x].father) {
+        if (tr[f].father != tar)
+            rotate(who(x) == who(f) ? f : x);
+        rotate(x);
+    }
+    if (tar == 0)
+        root = x;
+}
+
+typedef int iter;
+
+iter kth(int rk, int tar) {
+    int idx = root, cnt = 0;
+    while (true) {
+        done(idx);
+        if (tr[tr[idx].next[0]].sum >= rk) {
+            idx = tr[idx].next[0];
+        } else {
+            rk -= tr[tr[idx].next[0]].sum + 1;
+            if (rk <= 0) {
+                splay(idx, tar);
+                return idx;
+            }
+            idx = tr[idx].next[1];
+        }
+    }
+}
+
+void build(int n) {
+    for (int i = 1; i <= n; i++) {
+        tr[cnt_tr - 1].next[1] = cnt_tr;
+        tr[cnt_tr].father = cnt_tr - 1;
+        cnt_tr++;
+    }
+    for (int i = cnt_tr - 1; i >= 1; i--)
+        up(i);
+    root = 1;
+    cnt_tr = n + 1;
+}
+
+vector<int> out;
+void walk(int idx) {
+    done(idx);
+    if (tr[idx].next[0]) walk(tr[idx].next[0]);
+    out.push_back(idx - 1);
+    if (tr[idx].next[1]) walk(tr[idx].next[1]);
+}
+
+void solve() {
+    int n, q;
+    cin >> n >> q;
+    build(n + 2);
+    while (q--) {
+        int l, r;
+        cin >> l >> r;
+        kth(l, 0);
+        kth(r + 2, root);
+        tr[tr[tr[root].next[1]].next[0]].lazy ^= true;
+    }
+    walk(root);
+    for (int i = 1; i <= n; i++)
+        cout << out[i] << ' ';
+    cout << endl;
+}
