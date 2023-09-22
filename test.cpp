54065b3691d82c06e0bf35fe9c48b714959d3d26
Jamhus_tao <jamhus_tao@qq.com>
2023-09-22 20:57:36 +0800

    将超级快读的简易版与超级快读分离

--- test.cpp
+++ test.cpp
@@ -2,12 +2,54 @@
 // Start at 2022/10/6
 // Please using int32_t and int64_t to replace the int and long long.
 #include <bits/stdc++.h>
-#define int long long
+#define int int64_t
 #define endl '\n'
-// #pragma GCC optimize(3, "Ofast", "inline")
+#pragma GCC optimize(3, "Ofast", "inline")
 using namespace std;
 
+#define MAXSIZE (1ull << 10)
+char buf[MAXSIZE], *p1 = nullptr, *p2 = nullptr;
+#define isdigit(c) ('0' <= (c) and (c) <= '9')
+#define isblank(c) ((c) == ' ' or (c) == '\t' or (c) == '\r' or (c) == '\n')
+#ifdef LOCAL
+#define gc() getchar()
+#else
+#define gc() (p1 == p2 and (p2 = (p1 = buf) + fread(buf, 1, MAXSIZE, stdin), p1 == p2) ? '\0' : *p1++)
+#endif
+
+template <typename T>
+inline void read(T &x) {
+    x = 0;
+    char c = gc();
+    bool neg = false;
+    while (not isdigit(c)) {
+        neg = true;
+        c = gc();
+    }
+    while (isdigit(c)) {
+        x = (x << 3) + (x << 1) + (c ^ 48);
+        c = gc();
+    }
+    if (neg)
+        x = -x;
+}
+
+inline void read(char *s) {
+    char ch = gc();
+    for (; isblank(ch); ch = gc())
+        ;
+    for (; not isblank(ch); ch = gc())
+        *s++ = ch;
+    *s = '\0';
+}
+
 signed main() {
-    
+    int x;
+    read(x);
+    // cin >> x;
+    char s[1024];
+    read(s);
+    // cin >> s;
+    cout << x << ' ' << s << endl;
     return 0;
 }
--- 类_收集/超级快读.cpp
+++ 类_收集/超级快读.cpp
@@ -7,123 +7,38 @@
 #pragma GCC optimize(3, "Ofast", "inline")
 using namespace std;
 
-// 超级读写
-// @smallC Modified by GreatLiangpi
-// #define LOCAL 1  // 调试开关
-class IO {
-#define MAXSIZE (1 << 20)
-#define isdigit(x) ((x) >= '0' and (x) <= '9')
-#define isblank(x) ((x) == ' ' or (x) == '\n' or (x) == '\r' or (x) == '\t')
-
-   public:
-    char buf[MAXSIZE], *p1, *p2;
-    char pbuf[MAXSIZE], *pp;
-    bool eof = false;
-
+#define MAXSIZE (1ull << 10)
+char buf[MAXSIZE], *p1 = nullptr, *p2 = nullptr;
+#define isdigit(c) ('0' <= (c) and (c) <= '9')
+#define isblank(c) ((c) == ' ' or (c) == '\t' or (c) == '\r' or (c) == '\n')
 #ifdef LOCAL
+#define gc() getchar()
 #else
-    IO() : p1(buf), p2(buf), pp(pbuf) {}
-
-    ~IO() { fwrite(pbuf, 1, pp - pbuf, stdout); }
+#define gc() (p1 == p2 and (p2 = (p1 = buf) + fread(buf, 1, MAXSIZE, stdin), p1 == p2) ? '\0' : *p1++)
 #endif
 
-    friend IO &operator>>(IO &io, )
-
-    inline char gc() {
-#ifdef LOCAL  // 调试，可显示字符
-        return getchar();
-#else
-        if (p1 == p2)
-            p2 = (p1 = buf) + fread(buf, 1, MAXSIZE, stdin);
-        if (p1 == p2) {
-            eof = true;
-            return ' ';
-        }
-        return *p1++;
-#endif
-    }
-
-    inline void push(const char &c) {
-#ifdef LOCAL  // 调试，可显示字符
-        putchar(c);
-#else
-        if (pp - pbuf == MAXSIZE)
-            fwrite(pbuf, 1, MAXSIZE, stdout), pp = pbuf;
-        *pp++ = c;
-#endif
-    }
-
-    template <class T>
-    inline bool read(T &x) {
-        double tmp = 1;
-        bool sign = 0;
-        x = 0;
-        char ch = gc();
-        if (eof)
-            return false;
-        for (; not isdigit(ch); ch = gc())
-            if (ch == '-')
-                sign = 1;
-        for (; isdigit(ch); ch = gc())
-            x = x * 10 + (ch - '0');
-        if (ch == '.')
-            for (ch = gc(); isdigit(ch); ch = gc())
-                tmp /= 10.0, x += tmp * (ch - '0');
-        if (sign)
-            x = -x;
-        return not eof;
-    }
-
-    inline bool read(char *s) {
-        char ch = gc();
-        if (eof)
-            return false;
-        for (; isblank(ch); ch = gc())
-            ;
-        for (; not isblank(ch); ch = gc())
-            *s++ = ch;
-        *s = 0;
-        return not eof;
-    }
-
-    inline bool read(char &c) {
+template <typename T>
+inline void read(T &x) {
+    x = 0;
+    char c = gc();
+    bool neg = false;
+    while (not isdigit(c)) {
+        neg = true;
         c = gc();
-        if (eof)
-            return false;
-        for (; isblank(c); c = gc())
-            ;
-        return not eof;
     }
+    while (isdigit(c)) {
+        x = (x << 3) + (x << 1) + (c ^ 48);
+        c = gc();
+    }
+    if (neg)
+        x = -x;
+}
 
-    template <class T>
-    inline void write(T x) {
-        if (x < 0)
-            x = -x, push('-');  // 负数输出
-        static T sta[35];
-        T top = 0;
-        do {
-            sta[top++] = x % 10, x /= 10;
-        } while (x);
-        while (top)
-            push(sta[--top] + '0');
-    }
-
-    inline void write(const char *s) {
-        for (; *s; s++)
-            push(*s);
-    }
-
-    inline void write(char *s) {
-        write((const char *)s);
-    }
-
-    inline void write(const char &c) {
-        push(c);
-    }
-
-    template <class T>
-    inline void write(T x, const char &lastChar) {
-        write(x);
-        push(lastChar);
-    }
-} io;
+inline void read(char *s) {
+    char ch = gc();
+    for (; isblank(ch); ch = gc())
+        ;
+    for (; not isblank(ch); ch = gc())
+        *s++ = ch;
+    *s = '\0';
+}
