8ef2794114557ce3f0d6a25ca8ede2bd254853c3
jamhus-tao <jamhus_tao@qq.com>
2023-09-26 14:13:11 +0800

    修改了字符串哈希，提供宏选项。并修复其一个问题。

--- 字符串/Hash.cpp
+++ 字符串/Hash.cpp
@@ -10,93 +10,135 @@ using namespace std;
 // 字符串哈希
 const int MAX = 1000005;
 
-// 逆序开关: 启用逆序哈希, 包括回文判定
-#define HASH_REV
+#define HASH_DOUBLE  // 启用双模数哈希
+// #define HASH_NATURE  // 启用自然溢出哈希
+// #define HASH_RANDOM  // 启用随机哈希因子
+// #define HASH_REV     // 启用逆序哈希, 用于回文判定
 
-// 哈希因子集. 该数据集最多支持四哈
-const int64_t HU_SET[4][3] = {
-    {131, 157, 293},
-    {1019, 1471, 1667},
-    {10037, 13331, 16183},
-    {154590409516822759, 2305843009213693951, 4384957924686954497},
-};
-const int64_t HM_SET[] = {999999937, 1000000007, 1000000009};
-int H1 = 131, H2 = 13331, HM = 1000000007;  // 默认哈希因子
-
-int h1[MAX], p1[MAX];
-int h2[MAX], p2[MAX];
-#ifdef HASH_REV
-int g1[MAX];
-int g2[MAX];
+#ifdef HASH_DOUBLE
+const int MASK = (1ull << 32) - 1;
 #endif
 
-struct HashCode {
-    int hc1, hc2, len;
-    friend bool operator==(const HashCode &x, const HashCode &y) {
-        return x.hc1 == y.hc1 and x.hc2 == y.hc2 and x.len == y.len;
-    }
-    friend bool operator!=(const HashCode &x, const HashCode &y) {
-        return not(x == y);
-    }
-    // friend bool operator<(const HashCode &x, const HashCode &y) {
-    //     if (x.len != y.len)
-    //         return x.len < y.len;
-    //     else if (x.hc1 != y.hc1)
-    //         return x.hc1 < y.hc1;
-    //     else
-    //         return x.hc2 < y.hc2;
-    // }
+#ifdef HASH_NATURE
+#define mm1(x, m) (x)
+#define mm2(x, m) (x)
+#else
+#define mm1(x, m) ((x) % (m))
+#define mm2(x, m) (((x) % (m) + (m)) % (m))
+#endif
+
+// 哈希因子集. 该数据集最多支持四哈
+#ifdef HASH_RANDOM
+const int64_t HB_SET[] = {
+    131, 157, 293,
+    1019, 1471, 1667,
+    10037, 13331, 16183,
 };
+const int64_t HM_SET[] = {998244353, 999999937, 1000000007, 1000000009, 1004535809};
+int H1, H2, M1, M2;
+#else
+const int H1 = 131, H2 = 13331, M1 = 1000000007, M2 = 998244353;
+#endif
+
+int h1[MAX], p1[MAX];
+#ifdef HASH_DOUBLE
+int h2[MAX], p2[MAX];
+#endif
+#ifdef HASH_REV
+int g1[MAX];
+#ifdef HASH_DOUBLE
+int g2[MAX];
+#endif
+#endif
+
+typedef pair<int, int> HashCode;  // {HashCode, HashLength}
 
 // 初始化随机哈希因子
 void hash_init(int n) {
+#ifdef HASH_RANDOM
     srand(time(nullptr));
-    H1 = HU_SET[0][rand() % 3];
-    H2 = HU_SET[1][rand() % 3];
-    HM = HM_SET[rand() % 3];
-    p1[0] = 1, p2[0] = 1;
+    int SB = sizeof(HB_SET) / sizeof(HB_SET[0]), SM = sizeof(HM_SET) / sizeof(HM_SET[0]);
+    H1 = H2 = HB_SET[rand() % SB];
+    while (H1 == H2) H2 = HB_SET[rand() % SB];
+    M1 = M2 = HM_SET[rand() % SM];
+    while (M1 == M2) M2 = HM_SET[rand() % SM];
+#endif
+    p1[0] = 1;
+#ifdef HASH_DOUBLE
+    p2[0] = 1;
+#endif
     for (int i = 0; i < n; i++) {
-        p1[i + 1] = (p1[i] * H1) % HM;
-        p2[i + 1] = (p2[i] * H2) % HM;
+        p1[i + 1] = mm1(p1[i] * H1, M1);
+#ifdef HASH_DOUBLE
+        p2[i + 1] = mm1(p2[i] * H2, M2);
+#endif
     }
 }
 
 // 字符串哈希初始化
 void hash_it(const char *s, int n) {
-    h1[0] = 0, h2[0] = 0;
+    h1[0] = 0;
+#ifdef HASH_DOUBLE
+    h2[0] = 0;
+#endif
     for (int i = 0; i < n; i++) {
-        h1[i + 1] = (h1[i] * H1 + s[i]) % HM;
-        h2[i + 1] = (h2[i] * H2 + s[i]) % HM;
+        h1[i + 1] = mm1(h1[i] * H1 + s[i], M1);
+#ifdef HASH_DOUBLE
+        h2[i + 1] = mm1(h2[i] * H2 + s[i], M2);
+#endif
     }
 #ifdef HASH_REV
-    g1[n + 1] = 0, g2[n + 1] = 0;
+    g1[n + 1] = 0;
+#ifdef HASH_DOUBLE
+    g2[n + 1] = 0;
+#endif
     for (int i = n - 1; i >= 0; i--) {
-        g1[i + 1] = (g1[i + 2] * H1 + s[i]) % HM;
-        g2[i + 1] = (g2[i + 2] * H2 + s[i]) % HM;
+        g1[i + 1] = mm1(g1[i + 2] * H1 + s[i], M1);
+#ifdef HASH_DOUBLE
+        g2[i + 1] = mm1(g2[i + 2] * H2 + s[i], M2);
+#endif
     }
 #endif
 }
 
 // 截取子串哈希. 0-index, 左闭右闭
 HashCode hash_sub(int l, int r, bool reverse = false) {
-    if (l > r) swap(l, r);
 #ifdef HASH_REV
     if (reverse) {
-        int s1 = ((g1[l + 1] - g1[r + 2] * p1[r - l + 1]) % HM + HM) % HM;
-        int s2 = ((g2[l + 1] - g2[r + 2] * p2[r - l + 1]) % HM + HM) % HM;
-        return {s1, s2, r - l + 1};
+        int s1 = mm2(g1[l + 1] - g1[r + 2] * p1[r - l + 1], M1);
+#ifdef HASH_DOUBLE
+        int s2 = mm2(g2[l + 1] - g2[r + 2] * p2[r - l + 1], M2);
+        return {s1 << 32 | s2, r - l + 1};
+#else
+        return {s1, r - l + 1};
+#endif
     }
 #endif
-    int s1 = ((h1[r + 1] - h1[l] * p1[r - l + 1]) % HM + HM) % HM;
-    int s2 = ((h2[r + 1] - h2[l] * p2[r - l + 1]) % HM + HM) % HM;
-    return {s1, s2, r - l + 1};
+    int s1 = mm2(h1[r + 1] - h1[l] * p1[r - l + 1], M1);
+#ifdef HASH_DOUBLE
+    int s2 = mm2(h2[r + 1] - h2[l] * p2[r - l + 1], M2);
+    return {s1 << 32 | s2, r - l + 1};
+#else
+    return {s1, r - l + 1};
+#endif
 }
 
 // 连接子串哈希
 HashCode hash_join(HashCode lhc, HashCode rhc) {
-    int s1 = (lhc.hc1 * p1[rhc.len] + rhc.hc1) % HM;
-    int s2 = (lhc.hc2 * p2[rhc.len] + rhc.hc2) % HM;
-    return {s1, s2, lhc.len + rhc.len};
+#ifdef HASH_DOUBLE
+    int lh1 = lhc.first >> 32, lh2 = lhc.first & MASK, llen = lhc.second;
+    int rh1 = rhc.first >> 32, rh2 = rhc.first & MASK, rlen = rhc.second;
+#else
+    int lh1 = lhc.first, llen = lhc.second;
+    int rh1 = rhc.first, rlen = rhc.second;
+#endif
+    int s1 = mm1(lh1 * p1[rlen] + rh1, M1);
+#ifdef HASH_DOUBLE
+    int s2 = mm1(lh2 * p2[rlen] + rh2, M2);
+    return {s1 << 32 | s2, llen + rlen};
+#else
+    return {s1, llen + rlen};
+#endif
 }
 
 // 判断回文
@@ -105,54 +147,3 @@ bool is_palindrome(int l, int r) {
     return hash_sub(l, r) == hash_sub(l, r, true);
 }
 #endif
-
-// 哈希表
-// 由于字符串哈希呈随机分别，使用哈希表较 map 有极大优化。
-template <typename T, int D>
-class HashTable {
-   private:
-    pair<bool, pair<int, T>> data[D];
-
-    int hash(int x) { return x % D; }
-    int rehash(int x) { return x + 1; }
-
-   public:
-    class iterator {
-        pair<int, T> *ptr;
-
-       public:
-        iterator(pair<bool, pair<int, T>> data[], int x) : ptr(&data[x].second) {}
-        pair<int, T> *operator->() { return ptr; }
-        bool operator==(const iterator &oth) { return ptr == oth.ptr; }
-    };
-
-    T &operator[](int x) {
-        int hash_code = hash(x);
-        while (data[hash_code].first and data[hash_code].second.first != x) {
-            hash_code = rehash(hash_code);
-        }
-        return data[hash_code].second.second;
-    }
-
-    iterator end() { return iterator(data, D); }
-
-    void insert(const pair<int, T> &p) {
-        int hash_code = hash(p.first);
-        while (data[hash_code].first)
-            hash_code = rehash(hash_code);
-        data[hash_code].first = true;
-        data[hash_code].second = p;
-    }
-
-    iterator find(int x) {
-        int hash_code = hash(x);
-        while (true) {
-            if (not data[hash_code].first)
-                return end();
-            if (data[hash_code].second.first == x)
-                break;
-            hash_code = rehash(hash_code);
-        }
-        return iterator(data, hash_code);
-    }
-};
