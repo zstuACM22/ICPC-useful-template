199005865de5dc804f1626b9d8360dda8fea1cb1
smallC233 <you@example>
2023-09-27 16:55:14 +0800

    添加了函数 whole_hash_only，宏定义 HASH_JOIN_ON，修改了 hash_init

--- 字符串/Hash.cpp
+++ 字符串/Hash.cpp
@@ -14,6 +14,7 @@ const int MAX = 1000005;
 // #define HASH_NATURE  // 启用自然溢出哈希
 // #define HASH_RANDOM  // 启用随机哈希因子
 #define HASH_REV     // 启用逆序哈希, 用于回文判定
+// #define HASH_JOIN_ON // 启用 hash_join
 
 // 提取 h2
 #ifdef HASH_DOUBLE
@@ -52,10 +53,16 @@ int g2[MAX];
 #endif
 #endif
 
+#ifdef HASH_JOIN_ON
 typedef pair<int, int> HashCode;  // {HashCode, HashLength}
+#define hc(x, len) {(x), (len)}
+#else
+typedef int HashCode;
+#define hc(x, len) (x)
+#endif
 
-// 初始化随机哈希因子
-void hash_init(int n) {
+// 自动执行，初始化随机哈希因子，p1, p2 数组初始化
+int hash_init = []() {
 #ifdef HASH_RANDOM
     srand(time(nullptr));
     int SB = sizeof(HB_SET) / sizeof(HB_SET[0]), SM = sizeof(HM_SET) / sizeof(HM_SET[0]);
@@ -68,15 +75,34 @@ void hash_init(int n) {
 #ifdef HASH_DOUBLE
     p2[0] = 1;
 #endif
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < MAX; i++) {
         p1[i + 1] = mm1(p1[i] * H1, M1);
 #ifdef HASH_DOUBLE
         p2[i + 1] = mm1(p2[i] * H2, M2);
 #endif
     }
+    return 1;
+} ();
+// 只对字符串整体哈希，返回 int
+inline int whole_hash_only(const char *s, int n) {
+    int sum1 = 0;
+#ifdef HASH_DOUBLE
+    int sum2 = 0;
+#endif
+    for (int i = 0; i < n; ++i) {
+        sum1 = mm1(sum1 * H1 + s[i], M1);
+#ifdef HASH_DOUBLE
+        sum2 = mm1(sum2 * H2 + s[i], M2);
+#endif
+    }
+#ifdef HASH_DOUBLE
+    return sum1 << 32 | sum2;
+#else
+    return sum1;
+#endif
 }
 // 字符串哈希初始化
-void hash_it(const char *s, int n) {
+inline void hash_it(const char *s, int n) {
     h1[0] = 0;
 #ifdef HASH_DOUBLE
     h2[0] = 0;
@@ -100,30 +126,31 @@ void hash_it(const char *s, int n) {
     }
 #endif
 }
-// 截取子串哈希. 0-index, 左闭右闭
+// 截取子串哈希. 0-index, 左闭右闭 (填入值需满足 0 <= l, -1 <= r)
 template<bool reverse = false>
-HashCode hash_sub(int l, int r) {
+inline HashCode hash_sub(int l, int r) {
 #ifdef HASH_REV
     if (reverse) {
         int s1 = mm2(g1[l + 1] - g1[r + 2] * p1[r - l + 1], M1);
 #ifdef HASH_DOUBLE
         int s2 = mm2(g2[l + 1] - g2[r + 2] * p2[r - l + 1], M2);
-        return {s1 << 32 | s2, r - l + 1};
+        return hc(s1 << 32 | s2, r - l + 1);
 #else
-        return {s1, r - l + 1};
+        return hc(s1, r - l + 1);
 #endif
     }
 #endif
     int s1 = mm2(h1[r + 1] - h1[l] * p1[r - l + 1], M1);
 #ifdef HASH_DOUBLE
     int s2 = mm2(h2[r + 1] - h2[l] * p2[r - l + 1], M2);
-    return {s1 << 32 | s2, r - l + 1};
+    return hc(s1 << 32 | s2, r - l + 1);
 #else
-    return {s1, r - l + 1};
+    return hc(s1, r - l + 1);
 #endif
 }
 // 连接子串哈希
-HashCode hash_join(HashCode lhc, HashCode rhc) {
+#ifdef HASH_JOIN_ON
+inline HashCode hash_join(HashCode lhc, HashCode rhc) {
 #ifdef HASH_DOUBLE
     int lh1 = lhc.first >> 32, lh2 = lhc.first & MASK, llen = lhc.second;
     int rh1 = rhc.first >> 32, rh2 = rhc.first & MASK, rlen = rhc.second;
@@ -139,9 +166,10 @@ HashCode hash_join(HashCode lhc, HashCode rhc) {
     return {s1, llen + rlen};
 #endif
 }
+#endif
 // 判断回文
 #ifdef HASH_REV
-bool is_palindrome(int l, int r) {
+inline bool is_palindrome(int l, int r) {
     return hash_sub(l, r) == hash_sub<true>(l, r);
 }
 #endif
