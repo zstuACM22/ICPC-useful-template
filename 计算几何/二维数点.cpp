97135162bd57ef38fb8cb097d4bb0fa37cbbe653
jamhus-tao <jamhus_tao@qq.com>
2023-10-04 09:51:47 +0800

    修正二维数点的代码规范

--- 计算几何/二维数点.cpp
+++ 计算几何/二维数点.cpp
@@ -14,10 +14,10 @@ const int query_priority = -2;  // 修改/询问优先度开关
 /* 用法：
 0. 确定要开的空间大小：N 是点的个数，Q 是询问的矩阵个数，ED 是每个点 x 的右界（树状数组大小）
 （离散化 ED 是 N + 4 * Q, 不离散化 ED 由题目给出）（ED 本身需 +10 避免由 x++ 等情况带来的错误）
-1. 同一点，如果修改优先询问，query_priority = -2，询问优先修改，query_priority = 2 
+1. 同一点，如果修改优先询问，query_priority = -2，询问优先修改，query_priority = 2
 2. 如果 x 的范围特别大（或者有负数），需要打开离散化开关 #define DISCRETIZE，离散化 x
 3. 用 join_kind 加入表示种类的数组（x, y 可以为 0），可带权
-4. 用 join_suq 加入询问的区间
+4. 用 add_rectangle 加入询问的区间
 5. x 和 y 因为离散化/自增发生了改变，如果需要用坐标计算需要另外开变量记录真实值
 6. 直接 out 就可以输出每个矩阵内包含的点的个数了
 */
@@ -32,42 +32,31 @@ const int query_priority = -2;  // 修改/询问优先度开关
 
 class Count_point_2D {
    public:
-    void join_point(int x, int y, int val = 1) {
+    void add_point(int x, int y, int val = 1) {
         a[++cnt] = {x + 1, y + 1, val, query_priority};
     }
-    void join_suq(int x_1, int y_1, int x_2, int y_2) {
-        ++x_2, ++y_2;
-        a[++cnt] = {x_1, y_1, ++qcnt, 1};
+    void add_rectangle(int x_1, int y_1, int x_2, int y_2) {
+        ++x_2, ++y_2, ++qcnt;
+        a[++cnt] = {x_1, y_1, qcnt, 1};
         a[++cnt] = {x_1, y_2, qcnt, -1};
         a[++cnt] = {x_2, y_1, qcnt, -1};
         a[++cnt] = {x_2, y_2, qcnt, 1};
     }
-    void discretize() {
-        static int temp[N + Q * 4 + 10];
-        for (int i = 1; i <= cnt; ++i) {
-            temp[i] = a[i].x;
-        }
-        sort(temp + 1, temp + 1 + cnt);
-        int nt = unique(temp + 1, temp + 1 + cnt) - temp - 1;
-        for (int i = 1; i <= cnt; ++i) {
-            a[i].x = lower_bound(temp + 1, temp + 1 + nt, a[i].x) - temp;
-        }
-    }
     void solve() {
         solved = 1;
-        #ifdef DISCRETIZE
+#ifdef DISCRETIZE
         discretize();
-        #endif
+#endif
         sort(a + 1, a + 1 + cnt, [](const Node &a, const Node &b) {
             return a.y < b.y || (a.y == b.y && (a.x < b.x || (a.x == b.x && a.type < b.type)));
         });
         for (int i = 1; i <= cnt; ++i) {
             if (a[i].type == query_priority) {
-                #ifdef DISCRETIZE
+#ifdef DISCRETIZE
                 add(a[i].x, a[i].ind, cnt);
-                #else
+#else
                 add(a[i].x, a[i].ind, ED);
-                #endif
+#endif
                 continue;
             }
             ans[a[i].ind] += a[i].type * query(a[i].x);
@@ -83,16 +72,16 @@ class Count_point_2D {
     void init() {
         for (int i = 1; i <= cnt; ++i) {
             if (a[i].type == query_priority)
-                #ifdef DISCRETIZE
+#ifdef DISCRETIZE
                 add(a[i].x, -a[i].ind, cnt);
-                #else
+#else
                 add(a[i].x, -a[i].ind, ED);
-                #endif
+#endif
         }
         for (int i = 1; i <= qcnt; ++i) ans[i] = 0;
         cnt = qcnt = solved = 0;
     }
-    
+
    private:
     int cnt = 0, qcnt = 0;
     bool solved = 0;
@@ -101,20 +90,31 @@ class Count_point_2D {
         int x, y, ind, type;
     } a[N + Q * 4];
     int tr[ED + 10], ans[Q];
-    inline int lowbit(int x) {
+    int lowbit(int x) {
         return x & (-x);
     }
-    inline void add(int x, int v, int ed) {
+    void add(int x, int v, int ed) {
         for (int i = x; i <= ed; i += lowbit(i)) {
             tr[i] += v;
         }
     }
-    inline int query(int x) {
+    int query(int x) {
         int ans = 0;
         for (int i = x; i; i -= lowbit(i)) {
             ans += tr[i];
         }
         return ans;
+    }
+    void discretize() {
+        static int temp[N + Q * 4 + 10];
+        for (int i = 1; i <= cnt; ++i) {
+            temp[i] = a[i].x;
+        }
+        sort(temp + 1, temp + 1 + cnt);
+        int nt = unique(temp + 1, temp + 1 + cnt) - temp - 1;
+        for (int i = 1; i <= cnt; ++i) {
+            a[i].x = lower_bound(temp + 1, temp + 1 + nt, a[i].x) - temp;
+        }
     }
 } cp2d;
 
@@ -124,12 +124,12 @@ inline void solve() {
     for (int i = 1; i <= n; ++i) {
         int x, y, w;
         cin >> x >> y >> w;
-        cp2d.join_point(x, y, w);
+        cp2d.add_point(x, y, w);
     }
     for (int i = 1; i <= m; ++i) {
         int x_1, y_1, x_2, y_2;
         cin >> x_1 >> y_1 >> x_2 >> y_2;
-        cp2d.join_suq(x_1, y_1, x_2, y_2);
+        cp2d.add_rectangle(x_1, y_1, x_2, y_2);
     }
     cp2d.out();
-} 
+}
